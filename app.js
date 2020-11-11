56b716624133172f4d28249f4597e876056a2a33
dorsaf ferchichi <dorsaf.ferchichi@esprit.tn>
2020-11-11 23:04:10 +0100

    adding session

--- app.js
+++ app.js
@@ -16,79 +16,88 @@ var app = express();
 
 const url = 'mongodb://localhost:27017/coursera';
 const connect = mongoose.connect(url);
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 connect.then((db) => {
-  console.log("Connected correctly to server");
+	console.log("Connected correctly to server");
+	app.use(session({
+		name: 'session-id',
+		secret: '12345-67890-09876-54321',
+		saveUninitialized: false,
+		resave: false,
+		store: new FileStore()
+	}));
 
-  // view engine setup
-  app.set('views', path.join(__dirname, 'views'));
-  app.use(cookieParser('12345-67890-09876-54321'));
-  function auth(req, res, next) {
-    console.log(req.signedCookies)
-    if (!req.signedCookies.user) {
-      var authHeader = req.headers.authorization;
-      if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-        return;
-      }
-      var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-      var user = auth[0];
-      var pass = auth[1];
-      if (user == 'admin' && pass == 'password') {
-        res.cookie('user', 'admin', { signed: true });
-        next(); // authorized
-      } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-      }
-    }
-    else {
-      if (req.signedCookies.user === 'admin') {
-        next();
-      }
-      else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
-      }
-    }
-  }
+	// view engine setup
+	app.set('views', path.join(__dirname, 'views'));
 
-  app.use(auth);
-  app.set('view engine', 'jade');
-  app.use(logger('dev'));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.use(session({
+		name: 'session-id',
+		secret: '12345-67890-09876-54321',
+		saveUninitialized: false,
+		resave: false,
+		store: new FileStore()
+	}));
+
+	function auth(req, res, next) {
+		console.log(req.session);
+
+		if (!req.session.user) {
+			var authHeader = req.headers.authorization;
+			if (!authHeader) {
+				var err = new Error('You are not authenticated!');
+				res.setHeader('WWW-Authenticate', 'Basic');
+				err.status = 401;
+				next(err);
+				return;
+			}
+			var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+			var user = auth[0];
+			var pass = auth[1];
+			if (user == 'admin' && pass == 'password') {
+				req.session.user = 'admin';
+				next(); // authorized
+			} else {
+				var err = new Error('You are not authenticated!');
+				res.setHeader('WWW-Authenticate', 'Basic');
+				err.status = 401;
+				next(err);
+			}
+		}
+		else {
+			if (req.session.user === 'admin') {
+				console.log('req.session: ', req.session);
+				next();
+			}
+			else {
+				var err = new Error('You are not authenticated!');
+				err.status = 401;
+				next(err);
+			}
+		}
+	}
 
 
+	app.use('/', indexRouter);
+	app.use('/users', usersRouter);
+	app.use('/dishes', dishRouter);
+	app.use('/promotions', promoRouter);
+	app.use('/leaders', leaderRouter);
 
+	// catch 404 and forward to error handler
+	app.use(function (req, res, next) {
+		next(createError(404));
+	});
+	// error handler
+	app.use(function (err, req, res, next) {
+		// set locals, only providing error in development
+		res.locals.message = err.message;
+		res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  app.use('/', indexRouter);
-  app.use('/users', usersRouter);
-  app.use('/dishes', dishRouter);
-  app.use('/promotions', promoRouter);
-  app.use('/leaders', leaderRouter);
-
-  // catch 404 and forward to error handler
-  app.use(function (req, res, next) {
-    next(createError(404));
-  });
-  // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-  });
+		// render the error page
+		res.status(err.status || 500);
+		res.render('error');
+	});
 }, (err) => { console.log(err); });
 module.exports = app;
