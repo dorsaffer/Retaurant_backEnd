4ecb05d65ac79a0b88534651d7063fa109710124
dorsaf ferchichi <dorsaf.ferchichi@esprit.tn>
2020-11-12 00:35:43 +0100

    adding user auth with passport

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var leaderRouter = require('./routes/leaderRouter');
 const mongoose = require('mongoose');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./routes/authenticate');
 var app = express();
 
 
@@ -32,6 +34,22 @@ connect.then((db) => {
 	app.use('/', indexRouter);
 	app.use('/users', usersRouter);
 
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+	function authPassport(req, res, next) {
+		console.log(req.user);
+
+		if (!req.user) {
+			var err = new Error('You are not authenticated!');
+			err.status = 403;
+			next(err);
+		}
+		else {
+			next();
+		}
+	}
+
 	function auth(req, res, next) {
 		console.log(req.session);
 
@@ -52,8 +70,8 @@ connect.then((db) => {
 		}
 	}
 
-	app.use(auth);
-	app.set('view engine', 'jade');
+	//app.use(auth);
+	app.use(authPassport)
 	app.use(logger('dev'));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: false }));
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const bodyParser = require('body-parser');
 var User = require('../models/user');
+var passport = require('passport');
 
 router.use(bodyParser.json());
 
@@ -88,4 +89,35 @@ router.get('/logout', (req, res) => {
     next(err);
   }
 });
+
+/* auth with passport */
+
+router.post('/passportsignup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ err: err });
+      }
+      else {
+        passport.authenticate('local')(req, res, () => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ success: true, status: 'Registration Successful!' });
+        });
+      }
+    });
+});
+
+router.post('/passportlogin', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({ success: true, status: 'You are successfully logged in!' });
+});
+
+
+
+
+
 module.exports = router;
